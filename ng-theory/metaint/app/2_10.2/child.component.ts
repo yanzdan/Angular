e1c705eb74cf4db03c91464011d502928359b24b
yanzdan <42217485+yanzdan@users.noreply.github.com>
2018-08-14 08:00:49 +0300

    test commit

--- ng-theory/metaint/app/2_10.2/child.component.ts
+++ ng-theory/metaint/app/2_10.2/child.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class ChildComponent{
 
   @Input() userNameInChild:string;
+  // !!! Важно
+  // Имя єммитера должно иметь вид <имя_изменяемого_свойства>Change
   @Output() userNameInChildChange = new EventEmitter<string>();
   onNameChange(model: string){
 
